e849f3990e3772673abccbee154ac548e146b332
deepak mohan <krishna66827@outlook.com>
2017-08-19 15:01:36 +0530

    feature: completing post transaction with settle up orders

--- HyperStoreServiceAPP/Controllers/TransactionsController.cs
+++ HyperStoreServiceAPP/Controllers/TransactionsController.cs
@@ -79,19 +79,61 @@ namespace HyperStoreServiceAPP.Controllers
             {
                 return BadRequest(ModelState);
             }
-            var transaction = await transactionDTO.CreateNewTransactionAsync(db);
-            //TODO: SettleUP orders
             try
             {
+                var transaction = await transactionDTO.CreateNewTransactionAsync(db);
+                var settleUpOrders = SettleUpOrders(transaction);
                 await db.SaveChangesAsync();
+                return CreatedAtRoute("DefaultApi", new { id = transaction.TransactionId }, settleUpOrders);
             }
             catch (DbUpdateException)
             {
                 throw;
 
             }
-            return CreatedAtRoute("DefaultApi", new { id = transaction.TransactionId }, transaction);
         }
+
+        private List<SupplierOrder> SettleUpOrders(Transaction transaction)
+        {
+            List<SupplierOrder> settleUpSupplierOrder = new List<SupplierOrder>();
+            if (transaction.IsCredit)
+                throw new Exception(String.Format("While settling up the orders transaction {0} cannot be of type credit", transaction.TransactionId));
+            var partiallyPaidOrders = db.SupplierOrders.Where(so => so.SupplierId == transaction.SupplierId &&
+                                                                   so.BillAmount - so.PaidAmount > 0)
+                                                       .OrderBy(wo => wo.OrderDate);
+            var debitTransactionAmount = transaction.TransactionAmount;
+            foreach (var partiallyPaidOrder in partiallyPaidOrders)
+            {
+                if (debitTransactionAmount <= 0)
+                    break;
+                var remainingAmount = partiallyPaidOrder.BillAmount - partiallyPaidOrder.PaidAmount;
+                if (remainingAmount < 0)
+                    throw new Exception(string.Format("Supplier OrderNo {0}, Amount remaining to be paid: {1} cannot be less than zero", partiallyPaidOrder.SupplierOrderNo, remainingAmount));
+                decimal payingAmountForOrder = Math.Min(remainingAmount, debitTransactionAmount);
+                debitTransactionAmount -= payingAmountForOrder;
+                var IsOrderSettleUp = SettleUpOrder(partiallyPaidOrder, payingAmountForOrder);
+                settleUpSupplierOrder.Add(partiallyPaidOrder);
+                db.SupplierOrderTransactions.Add(new SupplierOrderTransaction
+                {
+                    SupplierOrderTransactionId = Guid.NewGuid(),
+                    SupplierOrderId = partiallyPaidOrder.SupplierOrderId,
+                    TransactionId = transaction.TransactionId,
+                    PaidAmount = payingAmountForOrder,
+                    IsPaymentComplete = IsOrderSettleUp
+                });
+            }
+            return settleUpSupplierOrder;
+        }
+
+        private bool SettleUpOrder(SupplierOrder supplierOrder, decimal settleUpAmount)
+        {
+            supplierOrder.PaidAmount += settleUpAmount;
+            db.Entry(supplierOrder).State = EntityState.Modified;
+            if (supplierOrder.PaidAmount == supplierOrder.BillAmount)
+                return true;
+            return false;
+        }
+
 
         // DELETE: api/Transactions/5
         [ResponseType(typeof(Transaction))]
